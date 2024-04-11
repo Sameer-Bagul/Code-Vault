fd97b1f1e07931ecd6fff0f33cdb4a17c76b9e81
sameer-bagul <sameerbagul2004@gmail.com>
2024-04-11 15:03:19 +0530

    working on the SQL notes info

--- info/org.sql
+++ info/org.sql
@@ -94,7 +94,134 @@ SELECT department, COUNT(department) from worker GROUP BY department; ---> this 
 -- MIN
 SELECT department, MIN(salary) FROM worker GROUP BY department;
 
+
+-- MAX 
+SELECT * department, MIN(salary) FROM worker GROUP BY department;
+
+-- SUM 
+
+SELECT * department, SUM(salary) FROM worker GROUP BY department;
+
+-- HAVING  
+-- select + where = filter the distinct / UNGROUPED data
+-- GROUP BY + HAVING -> filter the grouped data 
+
 --- having --> GROUP BY HAVING , HAVING works with GROUP BY 
 --? Q. department, count having more than 2 worker
---* A. 
-SELECT department, COUNT(department) FROM worker GROUP BY department HAVING COUNT department > 2 ;  
+--* A. SELECT department, COUNT(department) FROM worker GROUP BY department HAVING COUNT department > 2 ;
+
+
+
+
+------ALTER OPTIONS -------
+-- 1. change schema 
+-- 2. ADD 
+-- MODIFY
+-- CHANGE COLUMN
+-- DROP COLUMN 
+-- RENAME 
+
+
+CREATE TABLE account (
+    id int PRIMARY KEY ,
+    name VARCHAR(255) UNIQUE,
+    balance INT NOT NULL DEFAULT 0; 
+);
+
+
+-- ADD new COLUMN 
+ALTER TABLE account ADD interest FLOAT NOT NULL DEFAULT 0;
+
+-- MODIFY 
+ALTER TABLE account MODIFY interest DOUBLE NOT NULL DEFAULT 0;
+
+-- CHANGE COLUMN -> RENAME THE COLUM 
+ALTER TABLE account CHANGE COLUMN interest saving_interest FLOAT NOT NULL DEFAULT 0;
+
+-- DROP COLUMN
+ALTER TABLE account DROP COLUMN saving_interest;
+
+-- RENAME THE TABLE 
+ALTER TABLE account RENAME TO account_details;  
+
+
+
+
+
+--=============  DML  =============--
+-- UPDATE ,DELETE, REPLACE.
+
+--============= JOINS ==============--
+
+-- INNER JOIN  
+select C.*, O.* from customer AS C; 
+-- LEFT JOIN 
+-- RIGHT JOIN
+-- FULL JOIN
+-- SELF JOIN 
+
+
+-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+CREATE DATABASE company ;
+use company;
+CREATE TABLE project(
+    id int PRIMARY KEY ,
+    emp_id int,
+    name VARCHAR(255),
+    client_id int
+    FOREIGN KEY (client_id) REFERENCES client(client_id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id int PRIMARY key ,
+    fname VARCHAR(255),
+    lname VARCHAR(255),
+    age int ,
+    email_id varchar(255),
+    phone_no int ,
+    city varchar(255)    
+);
+
+CREATE TABLE client(
+    id int ,
+    first_name varchar(255),
+    last_name varchar(255),
+    email_id varchar(255),
+    phone_no int ,
+    city varchar(255),
+    emp_id int ,
+    FOREIGN KEY (emp_id) REFERENCES employee(emp_id) ON DELETE CASCADE
+);
+
+-- Lets use the joining concept on the above database.
+ -- INNER JOIN 
+-- Enlist all the employees ID's, name aalong with Project Allocated to them
+SELECT e.id, e.fname, e.lname, p.id, p.name FROM employee as e INNER JOIN project as p ON e.id = p.emp_id;
+
+-- fetch out all the employee ID's and their contact detail who have have been working from jaipur with clients name working in hyderabad
+SELECT e.id, e.email_id, e.phone_no , c.first_name , c.last_name from employee as e INNER JOIN client as c ON  e.id = c.emp_id WHERE e.city = 'jaipur' ANd c.city = 'hydarabad';
+
+
+-- LEFT JOIN 
+-- Fetch out each project allocated to each employee .
+SELECT * FROM employee as e 
+LEFT JOIN project as p on e.id - p.emp_id;
+
+-- right join
+-- List all the projects along with the employee's name and their and respective allocated email id
+SELECT p.id , p.name , e.fname, e.lname, e.email_id FROM employee as e 
+
+------------- INNER JOIN without the inne join user name ------------------
+SELECT e.id , e.fname, e.lname , p.id , p.name FROM employee as e, project as p WHERE e.id = p.emp_id;
+
+
+
+-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+--? SET operations on Database
+-- union , intersection , minus    
+--! syntax :
+--* SELECT * FROM table1 UNION SELECT * FROM table2;
